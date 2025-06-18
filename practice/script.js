4e14d82b7877838550c129388f27d6b73abd308b
Vishal Pant <vishal.pant1@gmail.com>
2025-06-18 13:35:12 +0530

    js basics review

---
+++ practice/script.js
@@ -0,0 +1,7 @@
+const myText = "I am a string";
+const newString = myText.replace("string", "sausage");
+console.log(newString);
+
+const myArray = ["I", "love", "chocolate", "frogs"];
+const madeAString = myArray.join("|");
+console.log(madeAString);
